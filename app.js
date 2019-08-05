3504a80c2d7142bb05857abf2654224a1246399b
Alice Roherty-Carrier <aliceroherty@gmail.com>
2019-08-05 02:27:05 -0300

    Fixes

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const single = (revs = 1) => {
             pin2.writeSync(pinValues[1]);
             pin3.writeSync(pinValues[2]);
             pin4.writeSync(pinValues[3]);
-            delay(1);
+            delay(0.5);
         });
     }
 }
@@ -76,7 +76,7 @@ const full = (revs = 1) => {
             pin2.write(pinValues[1]);
             pin3.write(pinValues[2]);
             pin4.write(pinValues[3]);
-            delay(1);
+            delay(0.5);
         });
     }
 }
@@ -88,7 +88,7 @@ const half = (revs = 1) => {
             pin2.write(pinValues[1]);
             pin3.write(pinValues[2]);
             pin4.write(pinValues[3]);
-            delay(1);
+            delay(0.25);
         });
     }
 }
