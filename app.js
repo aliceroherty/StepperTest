e40ebdb6e34b24bb4ca2646bd1a02970018bdf4b
Alice Roherty-Carrier <aliceroherty@gmail.com>
2019-08-05 01:00:54 -0300

    Fixes

--- app.js
+++ app.js
@@ -52,11 +52,12 @@ const halfStep = [
 const single = (revs = 1) => {
     for(i = 0; i < revs; i++) {
         singlePhase.forEach((pinValues) => {
+            pin1.write(pinValues[0]);
+            pin2.write(pinValues[1]);
+            pin3.write(pinValues[2]);
+            pin4.write(pinValues[3]);
             setTimeout(() => {
-                pin1.write(pinValues[0]);
-                pin2.write(pinValues[1]);
-                pin3.write(pinValues[2]);
-                pin4.write(pinValues[3]);
+                
             }, 1000);           
         });
     }
